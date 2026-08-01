b9c746db25c223d2d65f6b040e453fd8fd2b92d5
Dea Triyamanda <akutriyamanda@gmail.com>
2026-08-01 19:35:03 +0700

    Update logo dan departemen

--- sima-cic-backend/database/seeders/DepartemenSeeder.php
+++ sima-cic-backend/database/seeders/DepartemenSeeder.php
@@ -15,30 +15,19 @@ class DepartemenSeeder extends Seeder
     {
         // Pastikan tabel departemens sudah ada
         DB::table('departemens')->insert([
-            [
-                'nama_departemen' => 'IT & Sistem Informasi',
-                'deskripsi' => 'Pengembangan dan pemeliharaan sistem informasi perusahaan.',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'nama_departemen' => 'Pemasaran & Penjualan',
-                'deskripsi' => 'Strategi pemasaran, branding, dan penjualan produk.',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'nama_departemen' => 'SDM & Administrasi',
-                'deskripsi' => 'Pengelolaan sumber daya manusia dan administrasi umum.',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-            [
-                'nama_departemen' => 'Keuangan & Akuntansi',
-                'deskripsi' => 'Pengelolaan anggaran, pembukuan, dan laporan keuangan.',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
+            
+        [
+           'nama_departemen' => 'Tenaga Harian Lepas',
+           'deskripsi' => 'Karyawan dengan status tenaga harian lepas.',
+           'created_at' => Carbon::now(),
+           'updated_at' => Carbon::now(),
+        ],
+        [
+           'nama_departemen' => 'Karyawan',
+           'deskripsi' => 'Karyawan tetap maupun kontrak perusahaan.',
+           'created_at' => Carbon::now(),
+           'updated_at' => Carbon::now(),
+        ],
         ]);
     }
 }
